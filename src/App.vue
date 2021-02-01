bb900f22e3c7b20d0cacfffcdc57f16d9283669c
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-01 20:55:32 +0100

    More cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+<template></template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
 <style>
+body {
+  min-height: 100vh;
+}
+
+#app {
+  height: 100vh;
+  position: relative;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  line-height: 1.4;
+  letter-spacing: 0.5px;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
 }
 </style>
